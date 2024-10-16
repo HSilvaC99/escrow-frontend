c8bbfb85f2b994cb8d10cb50a847db68af9277eb
HSilvaC99 <hsilva@ucm.es>
2024-10-16 03:21:01 +0200

    Added theme switcher and finished send frontend

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "selector", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#1f2937",
+        dark: "#e5e7eb",
+        light_hover: "#d1d5db",
+        dark_hover: "#4b5563",
+      },
+    },
   },
   plugins: [],
 };
